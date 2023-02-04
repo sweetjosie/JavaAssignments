e252aed2d2e5d6ad13d67db5a0f644f91fc052a6
Josie <sweetjosieme@gmail.com>
2023-02-04 06:36:24 -0800

    Hello World!

---
+++ introductions/AboutMe.java
@@ -0,0 +1,6 @@
+public class AboutMe {
+    public static void main(String[] args){
+        System.out.println("My name is Coding Dojo \n I am 100 years old \n My hometown is Burbank, CA");
+
+    }
+}
