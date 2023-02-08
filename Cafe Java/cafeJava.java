7b36fc6ba6af6b9c6dc84631d9a9581bc5a42b3e
Josie <sweetjosieme@gmail.com>
2023-02-08 06:18:23 -0800

    Cafe Java

--- Cafe Java/cafeJava.java
+++ Cafe Java/cafeJava.java
@@ -3,8 +3,8 @@ public class CafeJava {
         // APP VARIABLES
         // Lines of text that will appear in the app. 
         String generalGreeting = "Welcome to Cafe Java, ";
-        String pendingMessage = ", your order will be ready shortly";
-        String readyMessage = ", your order is ready";
+        String pendingMessage = " your order will be ready shortly";
+        String readyMessage = " your order is ready";
         String displayTotalMessage = "Your total is $";
         
         // Menu variables (add yours below)
@@ -27,7 +27,38 @@ public class CafeJava {
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
-        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
-    	// ** Your customer interaction print statements will go here ** //
+        System.out.println(generalGreeting + customer1 ); // Displays "Welcome to Cafe Java, Cindhuri"
+        if(isReadyOrder1) {
+            System.out.println( customer1 + displayTotalMessage + dripPrice);
+        }
+            else{
+                 System.out.println( customer1 + pendingMessage);
+            }
+            System.out.println(generalGreeting + customer2);
+        if(isReadyOrder2) {
+            System.out.println( customer2 + readyMessage + displayTotalMessage + mochaPrice);
+        }
+            else{
+                 System.out.println( customer2 + pendingMessage);
+            }
+                System.out.println(generalGreeting + customer2);
+        if(isReadyOrder3) {
+            System.out.println( customer3 + readyMessage + displayTotalMessage + lattePrice);
+        }
+            else{
+                 System.out.println( customer3 + pendingMessage);
+            }
+                        System.out.println(generalGreeting + customer4);
+        if(isReadyOrder4) {
+            System.out.println( customer4 + readyMessage + displayTotalMessage + capuccinoPrice);
+        }
+            else{
+                 System.out.println( customer4 + pendingMessage);
+            }
+             System.out.println(" Sorry for our mistake "+ customer3 + displayTotalMessage + (lattePrice - dripPrice));
+        }
     }
-}
+        
+       
+    
+        
