bf1d6dff2e0c90333ca5fb03551379bbb4323b53
Josie <sweetjosieme@gmail.com>
2023-02-11 18:45:11 -0800

    final result

--- Cafe Business/CafeUtil.java
+++ Cafe Business/CafeUtil.java
@@ -5,40 +5,41 @@ import java.util.ArrayList;
 class CafeUtil  {
     public int getStreakGoal(){
         int weeksum= 0;
-        for( int i = 0 ; i<= 10 ; i++){
-          weeksum += 5;
-        //    System.out.println(i);
+        for( int i = 0 ; i <= 10 ; i++){ 
+          weeksum += i ;
+
         }
         return weeksum;
     }
 
     public double getOrderTotal(double[] lineItems){
      double pricesum = 0;
-    //    for(double i=0; i < lineItems.length; i++ ){
-    //       double  pricesum += lineItems;
-    //    }
-    //    return lineItems;
-    // what is missing?
+       for(double i=0; i < lineItems.length; i++ ){
+           pricesum += i;
+       }
+       return pricesum;
+    
 
-    // enhanced for loop= wrapper class variable : ArrayList/collection of items
+    // enhanced for loop= wrapper class variable : ArrayList name
     // condition : variable to hold the final result += variable from the loop
     // return varable holding the final total ( w1d2@19m)
-        for (Double values : lineItems){
-            pricesum += values;
-        }
-        return pricesum;
+        // for (Double values : lineItems){
+        //     pricesum += values;
+        // }
+        // return pricesum;
     }
     public void displayMenu(ArrayList<String> menu){
-        // String menu =   menu.get(); // to access an element in an ArrayList using an index <-- doesnt like this
-                // for (int i = 0; i < menu.size(); i++){
-                //      System.out.println( ""+ menu.get( i ) ); } // it prints the items in the menu but not the index position
-            int index = 0;
-            for (String menuItems : menu){
-                    System.out.println((index++) +" "+ menuItems);
-            }
+        // String menu =   menu.get(); // to access an element in an ArrayList using an index 
+                for (int i = 0; i < menu.size(); i++){
+                System.out.println( i + " "+ menu.get( i ) ); } 
+            // int index = 0;
+            // for (String menuItems : menu){
+            //         System.out.println((index++) +" "+ menuItems);
+            // }
+            // another solution
         
 
-        // System.out.println(menu);
+        System.out.println(menu);
     }
     
     public void addCustomer(ArrayList<String> customers){
